99be61d6e15dd6b292c68f75d7c8a330cd64bda3
Vrajesh <patelvrajeshjagdishbhai@gmail.com>
2024-02-26 23:55:06 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -10,26 +10,26 @@ resource "azurerm_resource_group" "ncpl-rg" {
 resource "azurerm_virtual_network" "ncpl-vn" {
   name                = "ncpl-vn"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
+  location            = azurerm_resource_group.ncpl-rg.location
   resource_group_name = azurerm_resource_group.example.name
 }
 
 resource "azurerm_subnet" "ncpl-subnet" {
   name                 = "ncpl-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.ncpl-rg.name
+  virtual_network_name = azurerm_virtual_network.ncpl-vn.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
   count               = 2
   name                = "ncpl-nic-${count.index}"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.ncpl-rg.location
+  resource_group_name = azurerm_resource_group.ncpl-rg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.ncpl-subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
@@ -37,9 +37,9 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_linux_virtual_machine" "ncpm-vm" {
   count                = 2
   name                 = "ncpl-vm-${count.index}"
-  location             = azurerm_resource_group.example.location
-  resource_group_name  = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example[count.index].id]
+  location             = azurerm_resource_group.ncpl-rg.location
+  resource_group_name  = azurerm_resource_group.ncpl-rg.name
+  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
   size                 = "Standard_B1ls"  # Minimum size VM
   admin_username       = "adminuser"
   admin_password       = "P@ssw0rd1234!"  # Change to your desired password
