55bc7787130540ed36bc6b3297a262846a5e7856
Vrajesh <patelvrajeshjagdishbhai@gmail.com>
2024-02-26 18:03:29 +0000

    Added backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "ncpl-terraform"
+    storage_account_name  = "ncplstorage"
+    container_name        = "container"
+    key                   = "terraform.tfstate"
+  }
+}
