51bd017ec5d65b2ab677d85b7e17b0598ffd6713
Vrajeshask <vrajesh.ask@gmail.com>
2024-02-29 11:39:29 -0500

    update the vm fils

--- main.tf
+++ main.tf
@@ -1,60 +1,72 @@
-provider "azurerm" {
-  features {}
+variable "prefix" {
+  default = "ncpl"
 }
 
 resource "azurerm_resource_group" "ncpl-rg" {
-  name     = "ncpl-rg"
-  location = "East US"  # Change to your desired region
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
 }
 
-resource "azurerm_virtual_network" "ncpl-vn" {
-  name                = "ncpl-vn"
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.ncpl-rg.location
   resource_group_name = azurerm_resource_group.ncpl-rg.name
 }
 
-resource "azurerm_subnet" "ncpl-subnet" {
-  name                 = "ncpl-subnet"
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
   resource_group_name  = azurerm_resource_group.ncpl-rg.name
-  virtual_network_name = azurerm_virtual_network.ncpl-vn.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  count               = 2
-  name                = "ncpl-nic-${count.index}"
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.ncpl-rg.location
   resource_group_name = azurerm_resource_group.ncpl-rg.name
 
   ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.ncpl-subnet.id
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "ncpm-vm" {
-  count                = 2
-  name                 = "ncpl-vm-${count.index}"
-  location             = azurerm_resource_group.ncpl-rg.location
-  resource_group_name  = azurerm_resource_group.ncpl-rg.name
-  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
-  size                 = "Standard_B1ls"  # Minimum size VM
-  admin_username       = "adminuser"
-  admin_password       = "P@ssw0rd1234!"  # Change to your desired password
-  computer_name        = "hostname-${count.index}"
-  disable_password_authentication = false
+resource "azurerm_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.ncpl-rg.location
+  resource_group_name   = azurerm_resource_group.ncpl-rg.name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
 
-  source_image_reference {
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
   }
 }
