15578b42541899ac85dcd3ed086be974f3ae6718
Vrajesh <patelvrajeshjagdishbhai@gmail.com>
2024-03-09 23:43:48 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,11 @@
 provider "azurerm" {
   features {}
+  subscription_id = "7aef6c4c-cd91-46ec-ad02-1e4ca07b28d4"
+  client_id       = "86330ecf-fef9-4cd7-bf84-c0d712aa44d5"
+  client_secret   = "62538497-197c-4c03-9a01-fb36e08f8726"
+  tenant_id       = "13dc7446-cd31-4979-b778-27a010ba78ab"
 }
+
 variable "prefix" {
   default = "ncpl"
 }
@@ -8,68 +13,4 @@ variable "prefix" {
 resource "azurerm_resource_group" "ncpl-rg" {
   name     = "${var.prefix}-resources"
   location = "West Europe"
-}
-
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.ncpl-rg.location
-  resource_group_name = azurerm_resource_group.ncpl-rg.name
-}
-
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.ncpl-rg.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.ncpl-rg.location
-  resource_group_name = azurerm_resource_group.ncpl-rg.name
-
-  ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.ncpl-rg.location
-  resource_group_name   = azurerm_resource_group.ncpl-rg.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
-
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-  tags = {
-    environment = "staging"
-  }
 }
